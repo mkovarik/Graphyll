a50380b6f5a7503521aaa32a5282878a3df2032d
Michael Kovarik <michaelkovarik@outlook.com>
2017-03-25 16:14:27 -0400

    Added Cargo.toml

--- src/graphyll.rs
+++ src/graphyll.rs
@@ -1,3 +1,28 @@
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader, YamlEmitter};
+
+
+struct BuildGraph {
+    
+}
 pub fn test() {
-  println!("It works!");
+    let s =
+"
+nodes:
+  - syscalls:
+      - kramdown src/post1.md > dest/post1.html
+    dependencies:
+      - src/post1.md
+    targets:
+      - dest/post1.html
+  - syscalls:
+      - kramdown src/post2.md > dest/post2.html
+    dependencies:
+      - src/post2.md
+    targets:
+      - dest/post2.html
+";
+    let docs = YamlLoader::load_from_str(s).unwrap();
+    let doc = &docs[0];
+    println!("{:?}", doc);
 }
