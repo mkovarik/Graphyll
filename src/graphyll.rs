7102b17f8b5d6efb2d862a5e0e6f63fa2e26f19c
Michael Kovarik <michaelkovarik@outlook.com>
2017-03-27 16:00:21 -0400

    Added Node and Graphyll data structure -- needs testing

--- src/graphyll.rs
+++ src/graphyll.rs
@@ -1,28 +1,54 @@
-extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
+use std::collections::{HashSet, HashMap};
 
-
-struct BuildGraph {
-    
+// A Node struct describes a step in the build process.
+#[derive(Eq, PartialEq, Hash)]
+struct Node {
+    id : i32,
+    dependencies: Vec<String>,
+    targets: Vec<String>,
+}
+impl Node {
+    fn process(&self) {
+        println!("Node {} processed", self.id);
+    }
+}
+struct Graph {
+    nodes : Vec<Node>,
+}
+impl Graph {
+    fn adj_nodes(&self, node : &Node) -> Vec<&Node> {
+        let mut neighbors = Vec::new();
+        for neighbor in &self.nodes {
+            let dependencies = &neighbor.dependencies;
+            let targets = &node.targets;
+            let dep_set : HashSet<_> = dependencies.iter().collect();
+            let targ_set : HashSet<_> = targets.iter().collect();
+            let intersect : HashSet<_>;
+            intersect = dep_set.intersection(&targ_set).collect();
+            if intersect.len() > 0 {
+                neighbors.push(neighbor);
+            }
+        }
+        neighbors
+    }
+    fn process(&self) {
+        let mut visited_nodes = HashSet::new();
+        for node in &self.nodes {
+            visited_nodes.insert(node);
+        }
+    }
+    fn traverse<'a>(&'a self, root : &'a Node, 
+                visited_nodes : &mut HashSet<&'a Node>) {
+        if visited_nodes.contains(root) {
+            return;
+        }
+        root.process();
+        visited_nodes.insert(root);
+        for neighbor in self.adj_nodes(root) {
+            self.traverse(neighbor, visited_nodes);
+        }
+    }
 }
 pub fn test() {
-    let s =
-"
-nodes:
-  - syscalls:
-      - kramdown src/post1.md > dest/post1.html
-    dependencies:
-      - src/post1.md
-    targets:
-      - dest/post1.html
-  - syscalls:
-      - kramdown src/post2.md > dest/post2.html
-    dependencies:
-      - src/post2.md
-    targets:
-      - dest/post2.html
-";
-    let docs = YamlLoader::load_from_str(s).unwrap();
-    let doc = &docs[0];
-    println!("{:?}", doc);
+    println!("butt hammer");
 }
